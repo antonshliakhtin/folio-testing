017e80a5a5aea8cd8357e40adabf8e3a8e1dad15
Anton Shliakhtin <anton_shliakhtin@epam.com>
2019-11-19 16:40:21 +0200

    [BOOKS} edited raml description, deleted unnecessary methods

--- src/main/java/org/folio/rest/impl/BooksImpl.java
+++ src/main/java/org/folio/rest/impl/BooksImpl.java
@@ -7,7 +7,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.folio.common.OkapiParams;
 import org.folio.rest.ResponseHelper;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.Book;
@@ -20,7 +19,6 @@ import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.function.Function;
 
-import static io.vertx.core.Future.succeededFuture;
 import static org.folio.rest.tools.utils.TenantTool.tenantId;
 
 public class BooksImpl implements Books {
@@ -42,7 +40,7 @@ public class BooksImpl implements Books {
     log.debug("getBooks");
     ResponseHelper.respond(bookService.getBooks(query, offset, limits, tenantId(okapiHeaders)),
       GetBooksResponse::respond200WithApplicationJson, asyncResultHandler, null);
-    asyncResultHandler.handle(Future.succeededFuture(GetBooksResponse.status(Response.Status.NOT_IMPLEMENTED).build()));
+
   }
 
   @Override
@@ -50,48 +48,11 @@ public class BooksImpl implements Books {
   public void postBooks(Book book, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     log.debug("postBooks");
 
-    succeededFuture()
-      .compose(object -> bookService.addBook(book, new OkapiParams(okapiHeaders)))
-      .map(handleSuccessfulPost())
-      .setHandler(asyncResultHandler);
+//    succeededFuture()
+//      .compose(object -> bookService.addBook(book, new OkapiParams(okapiHeaders)))
+//      .map(handleSuccessfulPost())
+//      .setHandler(asyncResultHandler);
     asyncResultHandler.handle(Future.succeededFuture(PostBooksResponse.status(Response.Status.NOT_IMPLEMENTED).build()));
-  }
-
-  @Override
-  @Validate
-  public void putBooks(String accessToken, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug("putBooks");
-
-  }
-
-  @Override
-  @Validate
-  public void deleteBooks(Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug("deleteBooks");
-
-  }
-
-  @Override
-  @Validate
-  public void getBooksById(String id, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug("getBooksById");
-
-    ResponseHelper.respond(bookService.getOneBook(id, tenantId(okapiHeaders)),
-      GetBooksByIdResponse::respond200WithApplicationJson, asyncResultHandler, null);
-  }
-
-  @Override
-  @Validate
-  public void putBooksById(String id, String accessToken, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug("getBooksById");
-
-  }
-
-  @Override
-  @Validate
-  public void deleteBooksById(String id, String accessToken, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug("deleteBooksById");
-
   }
 
   private Function<Book, Response> handleSuccessfulPost() {
